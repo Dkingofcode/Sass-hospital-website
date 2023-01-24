6d57a0ed90de322ae04d4a4f8aed13e9816b4fe9
Dkingofcode <doladepo128@gmail.com>
2023-01-24 10:15:18 +0000

    Added swiperjs to testimonials section

--- app.js
+++ app.js
@@ -13,7 +13,29 @@ closeNavBtn.addEventListener("click", () => {
     closeNavBtn.style.display = 'none';
 })
 
+// TESTIMONIALS SECTION (swiper js)
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    //Responsive Breakpoints
+    breakpoints: {
+        // when window width is >= 600px
+        600 : {
+            slidesPerView: 2
+        },
+
+        // when width is >=  600px
+       1024: {
+        slidesPerView: 3
+       } 
+    }
+  });
 
 
 
