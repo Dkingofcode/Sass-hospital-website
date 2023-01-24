58f5feae69a3223218b66732dcaf66d7ad1b79a3
Dkingofcode <doladepo128@gmail.com>
2023-01-24 07:09:24 +0000

    added button functionality

---
+++ app.js
@@ -0,0 +1,64 @@
+const navItems = document.querySelector("#nav__items");
+const openNavBtn = document.querySelector("#open__nav-btn");
+const closeNavBtn = document.querySelector("#close__nav-btn");
+
+openNavBtn.addEventListener('click', () => {
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
